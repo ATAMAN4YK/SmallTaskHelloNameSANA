3fcd183883c4a97045a2188326462b740a232aa6
Denys Atamanchuk <denys.atamanchyk@gmail.com>
2022-04-02 21:11:36 +0300

    HelloName Project

---
+++ HelloName/Controllers/MainController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using HelloName.Models;
+
+namespace HelloName.Controllers
+{
+    public class MainController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Hello(Person user)
+        {
+            return View("Hello", user);
+        }
+    }
+}
